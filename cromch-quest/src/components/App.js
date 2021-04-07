95702112b0ea4bc4f1032212486f21572cebc170
Spencer K <sometimestanley@gmail.com>
2021-04-06 22:12:51 -0700

    Basic Setup
    With default bootstrap themes and note libraries etc

---
+++ cromch-quest/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CromchBar from "./CromchBar";
+
+const App = () => (
+  <div className="App">
+    <CromchBar />
+    <h1>Hello, React!</h1>
+  </div>
+);
+
+export default App;
